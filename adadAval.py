353f1f8eaf75573658673d3b6f5fe559bafe0cba
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-09-02 22:48:42 +0430

    change adade avale to function

--- adadAval.py
+++ adadAval.py
@@ -1,11 +1,15 @@
-n = int(input('enter the range:'))
-k = 1
-count = 0
-while k <= n:
-    if n % k == 0:
-        count += 1
-    k += 1
-if count == 2:
-    print("yes")
-else:
-    print("no")
+def adadaval(n):
+    k = 1
+    count = 0
+    while k <= n:
+        if n % k == 0:
+            count += 1
+        k += 1
+    if count == 2:
+        return True
+    else:
+        return False
+
+
+n = int(input('enter a number:'))
+print(adadaval(n))
