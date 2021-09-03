e4eb120e2122dd06adaee5bacc7137d59172be65
danialNouri <danial.nouri20@gmail.com>
2021-09-03 16:35:00 +0430

    mohasebe mohit va masahat morabe

---
+++ morabe.py
@@ -0,0 +1,11 @@
+def area(r):
+    return  r * r
+
+
+def environment(r):
+    return 4* r
+
+
+r = int(input("megdar zele morabe ra vared knid:"))
+print("masahat:", + area(r))
+print("mohit:", +environment(r))
