1caf58be9dd318676d3027dba48345a40c7651fb
danialNouri <danial.nouri20@gmail.com>
2021-09-03 16:43:42 +0430

    mohasebe mohit va masahat beyzi

---
+++ beyzi.py
@@ -0,0 +1,8 @@
+def area(r, R):
+    return 3.14 * r * R
+
+
+r = int(input("megdar shoae kuchak beyzi ra vared knid:"))
+R = int(input("megdar shoae bozorg beyzi ra vared knid:"))
+
+print("masahat:", + area(r, R))
