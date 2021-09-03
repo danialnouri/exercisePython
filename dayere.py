ad1d72f501c024c89bcf1ed5de8667e4559c0b2c
danialNouri <danial.nouri20@gmail.com>
2021-09-03 16:33:12 +0430

    mohasebe mohit va masahat dayere

---
+++ dayere.py
@@ -0,0 +1,11 @@
+def area(r):
+    return 3.14 * r * r
+
+
+def environment(r):
+    return 2 * 3.14 * r
+
+
+r = int(input("megdar shoae dayere ra vared knid:"))
+print("masahat:", + area(r))
+print("mohit:", +environment(r))
