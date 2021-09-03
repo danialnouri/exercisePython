4eaaf7724355ed6b5cc9f32496b35b9548114ae5
danialNouri <danial.nouri20@gmail.com>
2021-09-03 16:46:45 +0430

    mohasebe mohit va masahat kore

---
+++ kore.py
@@ -0,0 +1,11 @@
+def area(r):
+    return 3.14 * r * r* 4
+
+
+def volume(r):
+    return 4 * 3.14 * r*r*r/3
+
+
+r = int(input("megdar shoae kore ra vared knid:"))
+print("masahat:", + area(r))
+print("hajm:", +volume(r))
