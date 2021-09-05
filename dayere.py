8d459825c54098a395c99066537bdf49e83647dd
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-09-05 22:28:54 +0430

    add lib math and pi

--- dayere.py
+++ dayere.py
@@ -1,11 +1,14 @@
+import math
+
+
 def area(r):
-    return 3.14 * r * r
+    return math.pi * r ** 2
 
 
 def environment(r):
-    return 2 * 3.14 * r
+    return 2 * math.pi * r
 
 
 r = int(input("megdar shoae dayere ra vared knid:"))
-print("masahat:", + area(r))
-print("mohit:", +environment(r))
+print("masahat:",  area(r))
+print("mohit:", environment(r))
